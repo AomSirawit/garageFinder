40b7946bb2bc5a4773abe861b087fae1254171cf
AomSirawit <makdeesirawit@gmail.com>
2024-01-06 00:36:29 +0700

    rename button
    Rename button Submit

--- src/Components/Section4.jsx
+++ src/Components/Section4.jsx
@@ -35,7 +35,7 @@ function Section4() {
           <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading" />
         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" onClick={sendButton}>
-          Button
+          Submit
         </button>
       </div>
       
